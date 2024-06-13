2d90fe40c987282b06aa541e315b8092dc8b1a13
LUCAS MARTINO <LUCAS.MARTINO@correo.ucu.edu.uy>
2024-06-13 19:11:46 -0300

    Rules

---
+++ Front/PencaCopaAmerica2024/src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+            <li class="nav-item"><router-link to="/rules" class="nav-link px-2 text-white">Rules</router-link></li>
+        </ul>
+        <p class="text-center text-white">&copy; 2024 Penca UCU</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Front/PencaCopaAmerica2024/src/components/Rules.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container mt-4">
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">Rules</h1>
+        <p class="card-text">Points are calculated based on the result of the 90-minute match (including stoppage time). (does not include extra time or penalties)</p>
+        <p class="card-text">In case of a tie in points between users, the one with the highest number of exact results will win.</p>
+        <p class="card-text">The ranking of each group will be updated a few minutes after the end of the match.</p>
+        <p class="card-text">Points are awarded based on the following rules:</p>
+        <ul class="card-text">
+          <li>4 points for guessing the exact result!</li>
+          <li>2 points for guessing the winning team or a draw!</li>
+          <li>10 points for Champion</li>
+          <li>5 points for Sub-Champion</li>
+        </ul>
+        <p class="card-text">The prediction can be entered up to 60 minutes before the start of each match.</p>
+        <router-link to="/index" class="btn btn-primary button back-button"><img :src="require('@/assets/Icons/backarrow.png')" alt="Back to Index" /></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable vue/multi-word-component-names */
+export default {
+  name: 'Rules'
+}
+</script>
+
+<style scoped>
+.card-text {
+  text-align: justify;
+}
+
+.button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #1abc9c;
+  color: white;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #003c30;
+}
+
+.back-button img {
+  width: 24px;
+  height: 24px;
+}
+
+.card {
+  border-radius: 30px;
+  background: #e0e0e0;
+  box-shadow: 15px 15px 30px #bebebe,
+             -15px -15px 30px #ffffff;
+}
+</style>
